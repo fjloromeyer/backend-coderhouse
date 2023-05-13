1ad564c202f02e044c0bf2278f3ecd4dbae92351
Federico Loro Meyer <fjloromeyer@gmail.com>
2023-05-13 13:08:43 +0200

    desafio2 done

--- desafio2.js
+++ desafio2.js
@@ -5,27 +5,42 @@ class ProductManager {
     }
     
     //this function should be working ok.
-    validateCode(arr){
-        if(arr.length){
-          let codeLast = arr.reduce((p,c)=> {
-            return c.code > p ? c.code : p;
+    validateId(arrProducts){
+        if(arrProducts.length){
+          let idLast = arrProducts.reduce((p,c)=> {
+            return c.id > p ? c.id : p;
           }, 0);
-          return codeLast + 1;
+          return idLast + 1;
         }
         return 1;
       }
+
+    validateCode(arrProducts, code){
+      if (arrProducts.length > 0) {
+        let repeatedProduct = arrProducts.map(product => product.code == code)
+        return (repeatedProduct) ? false : true 
+      } else {
+        return true
+      }
+    }
     
-    //function to add products with its caracteristics. It receives an object.
+    //function to add products with its caracteristics. It receives an object (a product).
     addProduct (product) {
-        let { title, description, price, thumbnail, stock } = product;
+      let { title, description, price, thumbnail, code, stock } = product;
+      let insertedCode = this.validateCode(this.products, code)
+      if (insertedCode) {
         this.products.push({
-        title,
-        description,
-        price,
-        thumbnail,
-        code: this.validateCode(this.products),
-        stock
-        })
+            id: this.validateId(this.products),
+            title,
+            description,
+            price,
+            thumbnail,
+            code: code,
+            stock
+          })
+      } else {
+        console.log("ERROR: the code of the product is already taken by another product")
+      }
     }
 
     //function to get a the complete list of products.
@@ -34,11 +49,37 @@ class ProductManager {
     }
 
     //function to get a specific product by id (code).
-    getProductById(productCode) {
-      let productFound = this.products.map(product => product.code === productCode)
-      return (productFound) ? productFound : console.log("Not found")
+    getProductById(productId) {
+      let productFound = this.products.filter(product => product.id == productId)
+      // console.log(productFound)
+      return (productFound && productFound.length > 0) ?
+        console.log(productFound)
+        :
+        console.log("Not found")
     }
 }
 
-let test = new ProductManager('manzana')
-console.log(test.products)
+// TESTING PROCESS BELOW
+
+let test = new ProductManager()
+test.getProducts()
+test.addProduct({
+  title: 'producto prueba',
+  description: 'este es un producto prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25
+})
+test.getProducts()
+test.addProduct({
+  title: 'producto prueba',
+  description: 'este es un producto prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25
+})
+
+test.getProductById(1)
+test.getProductById(2)
